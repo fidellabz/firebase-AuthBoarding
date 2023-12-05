744bc02447b6b248797c92be3037ab50a93b63b7
Fidelis Ogbeni <144154732+fidellabz@users.noreply.github.com>
2023-12-05 11:25:17 +0000

    update script

--- script.js
+++ script.js
@@ -8,10 +8,13 @@
     firebase.initializeApp(firebaseConfig);
 
     //Get Elements
+    const txtName = document.getElementById("txtName");
     const txtEmail = document.getElementById("txtEmail");
+    const txtPhone = document.getElementById("txtPhone");
     const txtPassword = document.getElementById("txtPassword");
     const btnLogin = document.getElementById("btnLogin");
     const btnSignup = document.getElementById("btnSignup");
+    const loadingSpinner = document.getElementById('loadingSpinner');
 
     //Add Login Event
     btnLogin.addEventListener('click', e => {
@@ -29,23 +32,50 @@
         
     });
 
-    //Add Signup Event
-    btnSignup.addEventListener('click', e => {
+    // Assuming btnSignup, txtName, txtEmail, txtPhone, txtPassword, loadingSpinner, signupButton, firebase, and $ are defined elsewhere in your code
 
-        //get email and password
-        const email = txtEmail.value;
-        const password = txtPassword.value;
+// Add Signup Event
+btnSignup.addEventListener('click', e => {
+    // Get name, email, phone, password, and date
+    const name = txtName.value;
+    const email = txtEmail.value;
+    const phone = txtPhone.value;
+    const password = txtPassword.value;
+    const date = Date.now();
+
+    // Basic form validation
+    if (name && email && phone && password) {
+        // Show loading spinner
+        loadingSpinner.classList.remove('d-none');
+        btnSignup.setAttribute('disabled', 'true');
 
         const auth = firebase.auth();
 
-        //sign in with firebase auth
-        const promise = auth.createUserWithEmailAndPassword(email, password).then(user => {
-            alert("Signup Successful :)")
-        }).catch(err => {
-            alert(err.message);
-        });
+        // Sign up with Firebase auth
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(userCredential => {
+                // Hide loading spinner
+                loadingSpinner.classList.add('d-none');
+                btnSignup.removeAttribute('disabled');
 
-    });
+                // Show signup successful modal
+                $('#signupModal').modal('show');
+
+                alert("Signup Successful :)");
+            })
+            .catch(error => {
+                // Hide loading spinner
+                loadingSpinner.classList.add('d-none');
+                btnSignup.removeAttribute('disabled');
+
+                alert(error.message);
+            });
+    } else {
+        // Handle form validation error
+        alert("Please fill in all fields.");
+    }
+});
+
 
 
 }());
